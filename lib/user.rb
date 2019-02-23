49c8d2159a2069181c9ada58d93364d5f8a745cf
Vytautas Klimavicius <vyklimavicius@hotmail.com>
2019-02-23 14:08:26 -0500

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,13 @@
+class User
+
+  attr_accessor :first_name, :last_name #Setter/Getter
+
+  
+  # def initialize(first_name,last_name)
+  #   @first_name = first_name
+  #   @last_name = last_name
+  # end
+
+
+
+end
